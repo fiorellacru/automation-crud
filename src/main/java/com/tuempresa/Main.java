803a2be23ac24dcbdeb35fd7a2bb070ad009b73e
Bruno Avila <batribeno@latam.stefanini.com>
2026-05-06 01:08:04 -0500

    especificamente el print

--- src/main/java/com/tuempresa/Main.java
+++ src/main/java/com/tuempresa/Main.java
@@ -2,6 +2,6 @@ package com.tuempresa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola perra");
+        System.out.println("Hola bebitaaa");
     }
 }
