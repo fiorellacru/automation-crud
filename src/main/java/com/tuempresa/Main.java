6e645e0e60dc08d6772f126d5548db0aca1c89bc
Bruno Avila <batribeno@latam.stefanini.com>
2026-05-06 00:24:37 -0500

    prueba cambio

--- src/main/java/com/tuempresa/Main.java
+++ src/main/java/com/tuempresa/Main.java
@@ -2,6 +2,6 @@ package com.tuempresa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola loquita");
     }
 }
